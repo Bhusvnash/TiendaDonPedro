cb2a602e3364726566e6e2307152ae768ef95dc3
Dona <beharrysantoyo@gmail.com>
2026-06-10 12:56:12 -0500

    Se añadio MySql.Datta y modelos

---
+++ ProyectoVS_TiendaDonPedro/COMPLETE_FLAT_UI/BackEnd/modelos/ObjetoCliente.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMPLETE_FLAT_UI.BackEnd.modulos
+{
+		internal class Cliente
+		{
+				public long id_cliente { get; private set; }
+				public string nombre_cliente { get; private set; }
+				public string direccion_cliente { get; private set; }
+				public string email_cliente { get; private set; }
+
+				public Cliente(long id, string nombre, string direccion, string email)
+				{
+						id_cliente = id;
+						nombre_cliente = nombre;
+						direccion_cliente = direccion;
+						email_cliente = email;
+				}
+		}
+}
