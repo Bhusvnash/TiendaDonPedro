327e37e934323f87d3e5dab3f9873b2030fd75d5
Dona <beharrysantoyo@gmail.com>
2026-07-01 11:10:52 -0500

    init factura

---
+++ ProyectoVS_TiendaDonPedro/COMPLETE_FLAT_UI/BackEnd/funcionesFactura.cs
@@ -0,0 +1,40 @@
+﻿using COMPLETE_FLAT_UI.BackEnd.modelos;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMPLETE_FLAT_UI.BackEnd
+{
+		internal class funcionesFactura
+		{
+
+				public static bool newFactura( Factura factura)
+				{
+						//conexion 
+						
+						try
+						{
+
+								using(var conexion = new MySqlConnection(FuncLogin.cadenaConexion))
+								{
+										conexion.Open();
+										//tbl_facturaventa
+										//  | fecha_factura | id_cliente | total_factura | estado_factura
+									   string consulta = @"Insert Into tbl_facturaventa 
+									   (fecha_factura, id_cliente, total_factura, estado_factura)
+									   values  (@fecha, @id_cliente, @total, @estado)";
+									
+								}
+						}
+						catch
+						{
+								return false;
+						}
+
+				}
+
+		}
+}
---
+++ ProyectoVS_TiendaDonPedro/COMPLETE_FLAT_UI/BackEnd/modelos/ObjetoFactura.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMPLETE_FLAT_UI
+{
+		internal  class Factura
+		{
+				// id_factura | fecha_factura | id_cliente | total_factura | estado_factura
+				long id_factura { get; set; }
+				DateTime fecha_factura { get; set; }
+				long id_cliente { get; set; }
+				double total_factura { get; set; }
+				string estado_factura { get; set; }
+				//constructor
+				public Factura(long id_factura, DateTime fecha_factura, long id_cliente, double total_factura, string estado_factura)
+				{
+						this.id_factura = id_factura;
+						this.fecha_factura = fecha_factura;
+						this.id_cliente = id_cliente;
+						this.total_factura = total_factura;
+						this.estado_factura = estado_factura;
+				}
+
+		}
+}
